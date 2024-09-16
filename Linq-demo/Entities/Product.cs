63d2dd4ceda7b4ae39e55e34abbe3346192bd1a7
Juliana Melo <ferreirajuliana01234@gmail.com>
2024-09-16 18:16:20 -0300

    Adicionar arquivos de projeto.

---
+++ Linq-demo/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Linq_demo.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
